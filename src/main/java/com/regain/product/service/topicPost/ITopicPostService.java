3ce60f547d24bf7d6f27c1de1b891aa9b69ede54
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2025-06-17 23:02:04 +0700

    update

--- src/main/java/com/regain/product/service/topicPost/ITopicPostService.java
+++ src/main/java/com/regain/product/service/topicPost/ITopicPostService.java
@@ -1,4 +1,9 @@
 package com.regain.product.service.topicPost;
 
+import com.regain.product.model.TopicPost;
+
+import java.util.List;
+
 public interface ITopicPostService {
+    List<TopicPost> getTopicPosts();
 }
--- src/main/java/com/regain/product/service/topicPost/TopicPostServiceImpl.java
+++ src/main/java/com/regain/product/service/topicPost/TopicPostServiceImpl.java
@@ -1,4 +1,20 @@
 package com.regain.product.service.topicPost;
 
+import com.regain.product.model.TopicPost;
+import com.regain.product.repository.ITopicPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TopicPostServiceImpl implements ITopicPostService{
+    @Autowired
+    private ITopicPostRepository topicPostRepository;
+
+
+    @Override
+    public List<TopicPost> getTopicPosts() {
+        return topicPostRepository.findAll();
+    }
 }
